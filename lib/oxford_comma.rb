38c771f1dfaacfe5d98c7d5ef94d6f68ed9e2723
Britny Edwards <bredwards2@gmail.com>
2018-11-14 01:47:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
-  oxford_comma = "kiwi"
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
 
